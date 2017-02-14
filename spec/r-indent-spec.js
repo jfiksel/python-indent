2285598b0f6da5cc8ca3578a0a23cefedc8f361f
Jacob Fiksel <jfiksel@gmail.com>
2017-02-14 17:54:11 -0500

    Change filename in specs

--- spec/r-indent-spec.js
+++ spec/r-indent-spec.js
@@ -1,7 +1,7 @@
 "use babel";
 import RIndent from "../lib/r-indent";
 describe("r-indent", () => {
-    const FILE_NAME = "fixture.R";
+    const FILE_NAME = "fixture.r";
     let buffer = null;
     let editor = null;
     let rIndent = null;
